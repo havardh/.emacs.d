2cf4c8cd13121357a7d2f69e7c08870c8f1270d5
Håvard Wormdal Høiby <havard@Macbook.local>
2013-08-11 15:48:21 +0200

    Added norwegian mac keys fix

--- .emacs.d/setup/setup-mac.el
+++ .emacs.d/setup/setup-mac.el
@@ -3,5 +3,13 @@
 (setq mac-command-modifier 'meta)
 (setq ns-function-modifier 'hyper)
 
+; Norwegian keys
+(define-key key-translation-map (kbd "s-8") (kbd "["))
+(define-key key-translation-map (kbd "s-(") (kbd "{"))
+(define-key key-translation-map (kbd "s-9") (kbd "]"))
+(define-key key-translation-map (kbd "s-)") (kbd "}"))
+(define-key key-translation-map (kbd "s-7") (kbd "|"))
+(define-key key-translation-map (kbd "s-/") (kbd "\\"))
+(define-key key-translation-map (kbd "M-s-7") (kbd "M-|"))
 
 (provide 'setup-mac)

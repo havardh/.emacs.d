a6c7868e1cf38d342852470623e1bc4f886bdfd6
Håvard Wormdal Høiby <havard@Macbook.local>
2013-08-12 19:18:52 +0200

    Some fixes on autocomplete

--- .emacs.d/setup/setup-autocomplete.el
+++ .emacs.d/setup/setup-autocomplete.el
@@ -1,7 +1,11 @@
 (add-to-list 'load-path (expand-file-name "site-lisp/autocomplete" user-emacs-directory))
-
+;(defalias 'yas/current-snippet-table 'yas--get-snippet-tables)
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories (expand-file-name "site-lisp/autocomplete/ac-dict" user-emacs-directory))
 (ac-config-default)
+(setq ac-auto-start 0)
+(delq 'ac-source-yasnippet ac-sources)
+
+(global-auto-complete-mode t)
 
 (provide 'setup-autocomplete)

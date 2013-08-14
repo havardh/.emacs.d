9002bd4f9b5f0c75bd8f0f218c6a451b55b2c969
Håvard Wormdal Høiby <havard@Macbook.local>
2013-08-14 20:58:31 +0200

    Set tab vs spaces config in setup-projects.

--- .emacs.d/setup/setup-projects.el
+++ .emacs.d/setup/setup-projects.el
@@ -1,4 +1,6 @@
 (setq custom-persp/project-root "~/")
+(setq custom-persp/project-c-basic-offset 2)
+(setq custom-persp/project-indent-tabs-mode t)
 
 (defun custom-persp/emacs nil
   (interactive)
@@ -17,9 +19,27 @@
 (defun custom-persp/php nil
   (interactive)
   (setq custom-persp/project-root "~/php")
+  (setq custom-persp/project-c-basic-offset 2)
+  (setq custom-persp/project-indent-tabs-mode nil)
   (custom-persp "php"
 		(find-file custom-persp/project-root)))
 (define-key persp-mode-map (kbd "C-x p p") 'custom-persp/php)
-  
+
+(defun custom-persp/ibok nil
+  (interactive)
+  (setq custom-persp/project-root "/Users/havard/Documents/programmering/php/ibok")
+  (setq custom-persp/project-c-basic-offset 2)
+  (setq custom-persp/project-indent-tabs-mode t)
+  (custom-persp "ibok"
+		(find-file custom-persp/project-root)))
+(define-key persp-mode-map (kbd "C-x p i") 'custom-persp/ibok)
+
+(defun after-major-mode-hook nil
+  (interactive)
+  (setq c-basic-offset custom-persp/project-c-basic-offset)
+  (setq indent-tabs-mode custom-persp/project-indent-tabs-mode)
+  (setq tab-width custom-persp/project-c-basic-offset))
+
+(add-hook 'after-change-major-mode-hook 'after-major-mode-hook)
 
 (provide 'setup-projects)

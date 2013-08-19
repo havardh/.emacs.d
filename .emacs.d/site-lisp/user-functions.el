ba5bc44c2e90d63b656541e174d0f2d1f3d608ba
Håvard Wormdal Høiby <havard@Macbook.local>
2013-08-19 13:25:05 +0200

    Added open function

--- .emacs.d/site-lisp/user-functions.el
+++ .emacs.d/site-lisp/user-functions.el
@@ -2,4 +2,18 @@
   (interactive)
   (indent-region (point-min) (point-max)))
 
+(setq open-initial "")
+
+(defun open (&optional file)
+  (interactive)
+  (if (not file) (setq file (read-from-minibuffer "File to open: " open-initial)))
+  (setq open-initial file)
+  (shell-command (concat "open " file)))
+
+(defun compile-open nil
+  (interactive)
+  (compile nil)
+  (open))
+
+
 (provide 'user-functions)

a6dbcbf9206047b8e31779fbfdcbc30a8f9d2d78
Håvard Wormdal Høiby <havard@Macbook.local>
2013-08-20 12:08:58 +0200

    Added open and run functions

--- .emacs.d/site-lisp/user-functions.el
+++ .emacs.d/site-lisp/user-functions.el
@@ -15,5 +15,17 @@
   (compile nil)
   (open))
 
+(setq run-initial "")
+
+(defun run (&optional file) 
+  (interactive)
+  (if (not file) (setq file (read-from-minibuffer "File to run: " run-initial)))
+  (setq run-initial file)
+  (shell-command (concat "./" file)))
+
+(defun compile-run nil
+  (interactive)
+  (compile nil)
+  (open))
 
 (provide 'user-functions)

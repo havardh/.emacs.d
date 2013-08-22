a7288b3d37f236ad117f3fd11bdac3c6bebbb7d7
Håvard Wormdal Høiby <havard@Macbook.local>
2013-08-22 09:47:34 +0200

    Added magit-blame and ignore whitespace

--- .emacs.d/setup/setup-magit.el
+++ .emacs.d/setup/setup-magit.el
@@ -1,3 +1,24 @@
 (add-to-list 'load-path (expand-file-name "site-lisp/magit" user-emacs-directory))
 (require 'magit)
+(require 'magit-blame)
+
+;Ignore whitespace
+(defun magit-toggle-whitespace ()
+  (interactive)
+  (if (member "-w" magit-diff-options)
+      (magit-dont-ignore-whitespace)
+    (magit-ignore-whitespace)))
+
+(defun magit-ignore-whitespace ()
+  (interactive)
+  (add-to-list 'magit-diff-options "-w")
+  (magit-refresh))
+
+(defun magit-dont-ignore-whitespace ()
+  (interactive)
+  (setq magit-diff-options (remove "-w" magit-diff-options))
+  (magit-refresh))
+
+(define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)
+
 (provide 'setup-magit)

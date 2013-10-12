e484cd34a0fe6056b5a9daa7f911bc1890ec3600
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2013-10-12 10:39:08 +0200

    Fixed scroll, truncate and y-n

--- .emacs.d/setup/setup-user-preferences.el
+++ .emacs.d/setup/setup-user-preferences.el
@@ -1,3 +1,10 @@
 (delete-selection-mode 1)
+(scroll-bar-mode -1)
+(setq truncate-lines t)
+
+
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+(setq backup-directory-alist `(("." . "~/.backup")))
 
 (provide 'setup-user-preferences)
